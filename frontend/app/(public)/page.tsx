232e40eb2ba2090e88cca0302a33768935b60010
Vanja Dunkel <vanjushka@Vanjas-MacBook-Air.local>
2025-12-31 12:22:13 +0100

    Setup Next app router, fix dev environment, add landing page

---
+++ frontend/app/(public)/page.tsx
@@ -0,0 +1,3 @@
+export default function LandingPage() {
+    return <main style={{ padding: 24 }}>Landing</main>;
+}
---
+++ frontend/app/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from "next/font/google";
+export const inter = Inter({
+    subsets: ["latin"],
+});
+
+import { Cormorant_Garamond } from "next/font/google";
+export const cormorantGaramond = Cormorant_Garamond({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+});
